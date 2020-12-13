6b99dbec67f2428fa23f01f4b8ba9f4f133b7ab6
JaiverR01 <romerocw164@gmail.com>
2020-12-12 23:21:25 -0500

    prueba Will 333

--- seeders/20201114225211-seed-user.js
+++ seeders/20201114225211-seed-user.js
@@ -2,17 +2,17 @@
 
 module.exports = {
     up: async(queryInterface, Sequelize) => {
-        return queryInterface.bulkInsert('Users', [{
+        return queryInterface.bulkInsert('users', [{
             name: 'carlos',
             email: 'ejemplo@gmail.com',
-            password: '$2y$08$FTP/jKGNASwJf0ero7SBe.kQmUsOSjWYupPZ6/lS6en6RcithXFKO',
+            password: '$2y$12$hJ0ktbpE/C.fensE./3zreDEwpvmE0VxpnUzrPUJ6rPos7N6XDbnW', //contraseña:  micontraseña
             createdAt: new Date(),
             updatedAt: new Date()
         }]);
     },
 
     down: async(queryInterface, Sequelize) => {
-        return queryInterface.bulkDelete('Users', null, {});
+        return queryInterface.bulkDelete('users', null, {});
 
     }
 };
