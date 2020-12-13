e7e33d69cd657693f45e73fdd24f4fd27ddeeaa4
= <=>
2020-12-13 13:16:18 -0500

    Ttoal Backend

--- seeders/20201114225211-seed-user.js
+++ seeders/20201114225211-seed-user.js
@@ -5,7 +5,7 @@ module.exports = {
         return queryInterface.bulkInsert('users', [{
             name: 'carlos',
             email: 'ejemplo@gmail.com',
-            password: '$2y$12$hJ0ktbpE/C.fensE./3zreDEwpvmE0VxpnUzrPUJ6rPos7N6XDbnW', //contraseña:  micontraseña
+            password: '$2y$08$qG/CDfGlqWd/oKvglzFVb.2i9nn4XecwH7B/RV6iYcVB9fIZz2zk2', //contraseña:  micontraseña
             createdAt: new Date(),
             updatedAt: new Date()
         }]);
